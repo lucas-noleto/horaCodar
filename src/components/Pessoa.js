0524c0b197e1ffe91285b5c4697d69531b283745
lucas-noleto <lucas-noleto@outlook.com>
2024-08-03 22:20:04 -0300

    Versao alternativa do props

--- src/components/Pessoa.js
+++ src/components/Pessoa.js
@@ -1,10 +1,24 @@
-function Pessoa(props){
+//Versao alternativa
+//function Pessoa(props){
+//    return(
+//        <div>
+//            <img src= {props.foto} alt={props.nome}/>
+//            <h2>Nome: {props.nome}</h2>
+//            <p>Profissão: {props.profissao}</p>
+//            <p>Idade: {props.idade}</p>
+//        </div>
+//    )
+//}
+//
+//export default Pessoa
+
+function Pessoa({foto,nome,profissao,idade}){
     return(
         <div>
-            <img src= {props.foto} alt={props.nome}/>
-            <h2>Nome: {props.nome}</h2>
-            <p>Profissão: {props.profissao}</p>
-            <p>Idade: {props.idade}</p>
+            <img src= {foto} alt={nome}/>
+            <h2>Nome: {nome}</h2>
+            <p>Profissão: {profissao}</p>
+            <p>Idade: {idade}</p>
         </div>
     )
 }
