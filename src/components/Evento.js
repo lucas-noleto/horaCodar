fcc84d6f69567d9e76b5870c4c0a039f755bd0a3
lucas-noleto <lucas-noleto@outlook.com>
2024-08-08 14:32:21 -0300

    Metodos por props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,19 @@
-
+import Button from "./Button"
 
 function Evento (){
     function meuEvento(){
         console.log('Fui ativado!')
+
+    }
+
+    function segundoEvento(){
+        console.log('Ativando o segundo evento!')
     }
     return(
         <div>
             <p>Clique para disparar um evento:</p>
+            <Button event={meuEvento} text = "Primeiro evento"/>
+            <Button event={segundoEvento} text = "Segundo evento"/>
             <button onClick={meuEvento} >Ativar!</button>
         </div>
     )
