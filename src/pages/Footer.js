853ea0ef7fa01ba8c7f8f609504fc6596a4bec16
lucas-noleto <lucas-noleto@outlook.com>
2024-08-18 11:11:43 -0300

    React icons

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,6 +1,18 @@
+
+import {FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer (){
     return(
-        <footer>Rodape</footer>
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaLinkedin/></li>
+                <li><FaInstagram/></li>
+            </ul>
+            <p>Nosso rodape</p>
+        </footer>
+
     )
 }
 
