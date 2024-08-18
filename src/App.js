f8fb95b3d8944e64042db5d51016958db82369d6
lucas-noleto <lucas-noleto@outlook.com>
2024-08-18 10:00:22 -0300

    Routers

--- src/App.js
+++ src/App.js
@@ -10,32 +10,51 @@ import { useState } from 'react';
 import SeuNome from './components/SeuNome.js';
 import Saudacao from './components/Saudacao.js';
 
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Contato from './pages/Contatos.js';
+import Empresa from './pages/Empresa.js';
+import Navbar from './pages/Navbar.js';
+import Footer from './pages/Footer.js';
+
 function App() {
   const [nome,setNome] =  useState()
   const lista = ['React', 'Json','HTML']
 
   return (
-    <div className="App">
-      <HelloWorld />
-      <Pessoa nome = "Lucas" profissao="Engenheiro de Computação" idade = "29" foto = "https://via.placeholder.com/150"/>
 
-      <h2>Testando Eventos</h2>
-      <Evento/>
+    <Router>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/empresa" element={<Empresa />} />
+      <Route path="/contato" element={<Contato />} />
+    </Routes>
+    <Footer/>
+  </Router>
 
-      <Formulario />
+    // <div className="App">
+    //   <HelloWorld />
+    //   <Pessoa nome = "Lucas" profissao="Engenheiro de Computação" idade = "29" foto = "https://via.placeholder.com/150"/>
 
-      <h1>Renderizacao Condicional</h1>
-      <Condicional/>
+    //   <h2>Testando Eventos</h2>
+    //   <Evento/>
 
-      <h1>Renderizacao de listas</h1>
-      <RenderLista listas = {lista} />
+    //   <Formulario />
 
-      <h1>State Lift</h1>
-      <SeuNome setNome = {setNome}/>
-      <Saudacao nome = {nome}/>
+    //   <h1>Renderizacao Condicional</h1>
+    //   <Condicional/>
+
+    //   <h1>Renderizacao de listas</h1>
+    //   <RenderLista listas = {lista} />
+
+    //   <h1>State Lift</h1>
+    //   <SeuNome setNome = {setNome}/>
+    //   <Saudacao nome = {nome}/>
 
       
-    </div>
+  //   </div>
   );
 }
 
---
+++ src/pages/Contatos.js
@@ -0,0 +1,10 @@
+function Contato (){
+    return(
+        <div>
+            <h1>Contato</h1>
+            <p>Pagina de Contato</p>
+        </div>
+    )
+}
+
+export default Contato
